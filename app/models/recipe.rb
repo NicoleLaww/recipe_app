0f9999028b08e9d214433587c9a5f13222ff4cf4
marijkevanhell <jukevanhell@gmail.com>
2023-08-23 11:56:11 -0700

    Add validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user 
   has_many :favourites 
+  validates :title, presence: true
+  validates :time, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :number_of_servings, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :instructions, presence: true
+  validates :country, presence: true
+  validates :description, presence: true
+  validates :user_id, presence: true
+  validates :gluten_free, :vegan, :vegetarian, :breakfast, :kosher, :peanut_free, :halal, :dinner, :lunch, :dessert, :appetizer, presence: true #ensure they are marked as true or false, can't be null
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :favourites
   has_many :recipes
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 4 }
+  validates :username, presence: true, uniqueness: true
 end
