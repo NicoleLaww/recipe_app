3f821aae4d202f7499929180083887f0c00a0aff
Nicole Law <nicole.law12@gmail.com>
2023-08-29 21:57:41 -0400

    route organization recipes

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,10 @@ module RecipeWebsite
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.after_initialize do
+    ActiveRecord::Base.logger.level = Logger::DEBUG
+  
+    end
   end
 end
