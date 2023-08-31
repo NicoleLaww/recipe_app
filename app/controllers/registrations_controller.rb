cf03a8075f6c0cc5dd84a82e52fa263afdf4ef31
Nicole Law <nicole.law12@gmail.com>
2023-08-31 13:49:29 -0400

    rerouted after registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,7 +12,7 @@ class RegistrationsController < ApplicationController
     else
       @user = User.new(user_params)
       if @user.save 
-        redirect_to root_path, notice: 'User successfully registered.'
+        redirect_to login_path, notice: 'User successfully registered.'
       else 
         render :new 
       end
